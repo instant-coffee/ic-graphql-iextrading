eaa444ec7ae38416ee4b69a794d8853604552443
Lachlan MacKenzie <lachlanrm@gmail.com>
2018-04-16 13:15:27 -0700

    Add graphql endpoint

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
 
 const app = express()
 const PORT = 4001
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true
+}))
 
 app.get('/', function(req, res) {
   res.send('sup yo!');
